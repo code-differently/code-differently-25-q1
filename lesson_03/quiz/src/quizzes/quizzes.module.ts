747f0666773c8968b425c058701ede550406b081
bryanasb251 <bsingletonbarnhart251@gmail.com>
2025-03-11 12:48:20 +0000

    Fixed Import on Quiz

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { Bryanasingletonbarnhart } from './ bryana_singleton-barnhart_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz];
+const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, Bryanasingletonbarnhart];
 
 @Module({
   providers: [
