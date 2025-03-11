6cf2c88dc35d6d046436dc786ea3017911d1f11f
NelltheWiz <chutt251@gmail.com>
2025-03-11 21:00:51 +0000

    changes done

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,26 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
- Chutt_lesson_03
-import { ChanelHuttQuiz } from './Chanel_Hutt_quiz.js';
 
 import { Jbeyquiz } from './jbeyquiz.js';
 import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
-main
+import { ChanelHuttQuiz } from './Chanel_Hutt_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
- Chutt_lesson_03
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, ChanelHuttQuiz];
-=======
+
 const QUIZ_PROVIDERS = [
   AnthonyMaysQuiz,
   AnotherQuiz,
   MercedesMathewsQuiz,
   Jbeyquiz,
+  ChanelHuttQuiz,
 ];
-main
 
 @Module({
   providers: [
