a2662823272f2a603b4a3bbfe7a1e08f62ec8123
Dylan <dlafferty251@gmail.com>
2025-03-11 19:54:16 +0000

    I have created a new branch adding in Homework 3 because of a merge bug on my last branch

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { DylanLaffertyQuiz } from './dylan_lafferty_quiz.js';
 import { Jbeyquiz } from './jbeyquiz.js';
 import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
 import { RasheedMillerQuiz } from './rasheed_miller_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
-// Add your quiz provider here.
 const QUIZ_PROVIDERS = [
   AnthonyMaysQuiz,
   AnotherQuiz,
   MercedesMathewsQuiz,
   Jbeyquiz,
+  DylanLaffertyQuiz,
   RasheedMillerQuiz,
 ];
 
