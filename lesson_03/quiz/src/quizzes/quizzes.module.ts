5ddb19e49dea506dfe576233de1a0fa85f1d8220
jxwatson251 <jxwatson251@gmail.com>
2025-03-11 20:17:27 +0000

    lesson03

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { JasonWatsonQuiz } from './jason_watson_quiz.js';
 import { Jbeyquiz } from './jbeyquiz.js';
 import { MercedesMathewsQuiz } from './mercedes_mathews_quiz.js';
 import { RasheedMillerQuiz } from './rasheed_miller_quiz.js';
+
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -14,6 +16,7 @@ const QUIZ_PROVIDERS = [
   MercedesMathewsQuiz,
   Jbeyquiz,
   RasheedMillerQuiz,
+  JasonWatsonQuiz,
 ];
 
 @Module({
