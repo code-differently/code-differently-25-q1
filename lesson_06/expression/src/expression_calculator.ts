3a96919bebcfd99aa2b6dbc098cbe00197992f55
kksaunders251 <kksaunders251@gmail.com>
2025-03-17 20:27:15 +0000

    feat: implement calculation logic in ExpressionCalculator

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,22 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const base: number = this.add(b, c);
+    const expo: number = d;
+
+    return this.divide(this.multiply(a, this.pow(base, expo)), e);
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    return a / b;
   }
 }
