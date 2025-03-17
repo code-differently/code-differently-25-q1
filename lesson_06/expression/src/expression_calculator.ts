3d1c3641377469d056717e7251565c4b0d29154b
Olivia James <oxjames251@gmail.com>
2025-03-17 20:48:20 +0000

    feat: added calculate fuctions and helper functions

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,26 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const addition = this.add(a, b);
+    const exponent = this.pow(addition, c);
+    const multiplication = this.multiply(d, e);
+    const division = this.divide(exponent, multiplication);
+    return division;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
   }
 }
