d03f672a83e645d03783b81cd45b012b1ea3c5f6
Dylan <dlafferty251@gmail.com>
2025-03-17 20:12:28 +0000

    Feat/ Adds return function

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,7 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    return (a * this.pow(b + c, d)) / e;
   }
 
   pow(base: number, exponent: number): number {
