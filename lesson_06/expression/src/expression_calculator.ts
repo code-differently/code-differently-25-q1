74269d3101783b3d67f790f9409a0edf01f50292
jbey251 <jbey251@gmail.com>
2025-03-17 22:44:28 +0000

    feat: Created an expression calculator. The result is correct.

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,27 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    // (a + b) * c / Math.pow(d, e)
+    const additionVariable = this.add(a, b);
+    const multiplicationVariable = this.multiply(additionVariable, c);
+    const powerVariable = this.pow(d, e);
+    const dividingVariable = this.divide(multiplicationVariable, powerVariable);
+    return dividingVariable;
+  }
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  multiply(num1: number, num2: number): number {
+    return num1 * num2;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
   }
 }
