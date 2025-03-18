e4978e194ac66fcb202e2f0af6b833e11a2a18f1
NelltheWiz <chutt251@gmail.com>
2025-03-18 16:25:37 +0000

    feat/hw_06: added expression calculator

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,26 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    // a / b + c * Math.pow(d, e),
+    const division = this.divide(a, b);
+    const exponentation = this.pow(d, e);
+    const multiply = this.multiply(c, exponentation);
+    const result = this.add(division, multiply);
+    return result;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+  multiply(c: number, f: number): number {
+    return c * f;
+  }
+
+  add(num1: number, num2: number): number {
+    return num1 + num2;
   }
 }
