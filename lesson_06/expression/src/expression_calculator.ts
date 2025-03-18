fa057e6a48c9ab9a08e25dd0a9ef9b6c3d810c6f
Meiko-S22 <mdstephens251@gmail,.com>
2025-03-18 13:24:53 +0000

    feat: adds completed calculator fucntion

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,23 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const exponent = this.pow(a, b);
+    const addition = this.add(exponent, c);
+    const multiplication = this.multiply(addition, d);
+    const division = this.divide(multiplication, e);
+    return division;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    return a / b;
   }
 }
