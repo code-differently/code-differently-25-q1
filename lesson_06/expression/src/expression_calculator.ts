01e0cb6b6629cf9912a0a9339c58e684896d45db
jbey251 <jbey251@gmail.com>
2025-03-18 09:51:51 -0400

    chore: reverted expression calculator.
    Expression calculator is in a different pull request. This is the quiz branch.

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,27 +2,10 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    // (a + b) * c / Math.pow(d, e)
-    const additionVariable = this.add(a, b);
-    const multiplicationVariable = this.multiply(additionVariable, c);
-    const powerVariable = this.pow(d, e);
-    const dividingVariable = this.divide(multiplicationVariable, powerVariable);
-    return dividingVariable;
-  }
-
-  add(a: number, b: number): number {
-    return a + b;
-  }
-
-  multiply(num1: number, num2: number): number {
-    return num1 * num2;
+    return 0;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
-  }
-
-  divide(a: number, b: number): number {
-    return a / b;
   }
 }
