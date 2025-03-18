fc153c292de4394b48b28502f11ab430fd096290
jxwatson251 <jxwatson251@gmail.com>
2025-03-18 14:40:08 +0000

    update

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,11 +1,23 @@
 export class ExpressionCalculator {
-  /** Returns a calculation involving a, b, c, d, and e */
-  calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
-  }
-
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  /** Returns a calculation involving a, b, c, d, and e */
+  calculate(a: number, b: number, c: number, d: number, e: number): number {
+    const addition = this.add(a, b);
+    const division = this.divide(addition, c);
+    const exponent = this.pow(division, d);
+    const multiplication = this.multiply(exponent, e);
+    return multiplication;
   }
 }
