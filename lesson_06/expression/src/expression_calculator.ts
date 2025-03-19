fe834ed586c9acdba3c37db851d8089fc3c26766
Rasheed <rmillercohort251@gmail.com>
2025-03-19 11:45:21 +0000

    lesson_06_hw

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,11 +1,26 @@
 export class ExpressionCalculator {
-  /** Returns a calculation involving a, b, c, d, and e */
-  calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+  add(a: number, b: number): number {
+    return a + b;
   }
-
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
-}
+  divide(a: number, b: number): number {
+    /*if (denominator === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    */return a / b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  calculate(a: number, b: number, c: number, d: number, e: number): number {
+    const sumResult = this.add(a, b);
+    const divisionResult = this.divide(sumResult, c);
+    const powerResult = this.pow(c, d);
+    const multiplication = this.multiply(divisionResult, e);
+    console.log({ sumResult, powerResult, divisionResult, });
+    return multiplication
+  }
+  }
