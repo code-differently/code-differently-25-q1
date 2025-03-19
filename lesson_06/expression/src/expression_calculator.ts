249a1e49e9bcccf1d39d31781091fd6298311562
Evan Philakhong <evanthegamer2004@gmail.com>
2025-03-19 13:21:41 +0000

    refactor: added stored results in variables

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,11 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return this.divide(this.pow(this.add(a, b), c), this.multiply(d, e));
+    const base: number = this.add(a, b);
+    const num1: number = this.pow(base, c);
+    const num2: number = this.multiply(d, e);
+    const result: number = this.divide(num1, num2);
+    return result;
   }
 
   pow(base: number, exponent: number): number {
