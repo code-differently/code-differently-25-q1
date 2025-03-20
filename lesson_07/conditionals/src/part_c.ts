8c9b39bfa19535af6255cafbfe840df4ba9db7f5
mercedes-mathews <memathews251@gmail.com>
2025-03-20 00:55:23 +0000

    chore: adds completed functions for lesson7 and part_c

--- lesson_07/conditionals/src/part_c.ts
+++ lesson_07/conditionals/src/part_c.ts
@@ -6,6 +6,9 @@
  * @returns
  */
 export function isStrongPassword(password: string): boolean {
+  if (password.length >= 8) {
+    return /\d/.test(password) && /[A-Z]/.test(password);
+  }
   return false;
 }
 
@@ -16,7 +19,33 @@ export function isStrongPassword(password: string): boolean {
  * @returns
  */
 export function getDayOfWeek(day: number): string {
-  return "";
+  let result = "";
+  switch (day) {
+    case 0:
+      result = "Sunday";
+      break;
+    case 1:
+      result = "Monday";
+      break;
+    case 2:
+      result = "Tuesday";
+      break;
+    case 3:
+      result = "Wednesday";
+      break;
+    case 4:
+      result = "Thursday";
+      break;
+    case 5:
+      result = "Friday";
+      break;
+    case 6:
+      result = "Saturday";
+      break;
+    default:
+      result = "";
+  }
+  return result;
 }
 
 /**
@@ -31,5 +60,13 @@ export function getDayOfWeek(day: number): string {
  * @returns
  */
 export function getTicketPrice(age: number): number {
-  return 0;
+  if (age < 5) {
+    return 0;
+  } else if (age >= 5 && age <= 17) {
+    return 10;
+  } else if (age >= 18 && age <= 59) {
+    return 20;
+  } else {
+    return 15;
+  }
 }
