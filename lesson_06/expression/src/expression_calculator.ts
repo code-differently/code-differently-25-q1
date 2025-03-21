c5bad976107761f447cc903481778a68c676ed53
Dylan <dlafferty251@gmail.com>
2025-03-21 14:30:39 +0000

    FIX: I created a new branch and added calc

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,11 +1,24 @@
 export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
-  calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    if (b === 0) throw new Error("Cannot divide by zero");
+    return a / b;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+
+  calculate(a: number, b: number, c: number, d: number, e: number): number {
+    return this.multiply(a, this.divide(this.pow(this.add(b, c), d), e));
   }
 }
