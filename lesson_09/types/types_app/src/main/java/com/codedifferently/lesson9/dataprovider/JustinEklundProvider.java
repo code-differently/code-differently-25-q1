6bbd2974c7d626278439c30292ee1d3c34bdcd92
JEKLUND251 <JEKLUND251@gmail.com>
2025-03-25 14:11:02 +0000

    refixed errors occuring with push request

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/JustinEklundProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/JustinEklundProvider.java
@@ -1,11 +1,10 @@
 package com.codedifferently.lesson9.dataprovider;
 
 import java.util.Map;
-
 import org.springframework.stereotype.Service;
 
 @Service
-public class justineklund extends DataProvider {
+public class JustinEklundProvider extends DataProvider {
   public String getProviderName() {
     return "justineklund";
   }
