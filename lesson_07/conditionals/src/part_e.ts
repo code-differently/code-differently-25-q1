7de5ed86dd7b5645d2b3339a47f648c0f898281e
JEKLUND251 <JEKLUND251@gmail.com>
2025-03-25 13:07:04 +0000

    readded work into lesson 7

--- lesson_07/conditionals/src/part_e.ts
+++ lesson_07/conditionals/src/part_e.ts
@@ -1,84 +1,44 @@
-import { computeLexicographicDistance } from "./util.js";
+/**
+ * Write a function that takes a single character as an argument and
+ * returns boolean value true if the character is an uppercase letter.
+ *
+ * @param char
+ * @returns
+ */
+export function isUppercase(char: string): boolean {
+  if (char >= "A" && char <= "Z") {
+    return true;
+  }
+
+  return false;
+}
 
 /**
- * Compares two strings lexicographically.
+ * Determine if a person is eligible for a driving license (age and test passed).
  *
- * @param a The first `string` to compare.
- * @param b The second `string` to compare.
- * @return -1 if a is less than b, 1 if a is greater than b, and 0 otherwise.
+ * @param age
+ * @param passedTest
+ * @returns
  */
-export function compareStrings(a: string, b: string): number {
-  // The distance will be a number less than 0 if string `a` is lexicographically less than `b`, 1
-  // if it is greater, and 0 if the strings are equal.
-  const distance = computeLexicographicDistance(a, b);
+export function canGetDriverLicense(age: number, passedTest: boolean): boolean {
+  if (age >= 18 && passedTest) {
+    return true;
+  }
+  return false;
+}
 
-  if (distance < 0) {
-    return -1;
-  } else if (distance > 0) {
-    return 1;
+/**
+ * Check if a store is open based on the day and time. The store is open
+ * Monday to Saturday from 9 AM to 9 PM.
+ *
+ * @param day
+ * @param hour
+ * @returns
+ */
+export function isStoreOpen(day: string, hour: number): boolean {
+  if (day === "Sunday" || hour < 9 || hour >= 21) {
+    return false;
   } else {
-    return 0;
+    return true;
   }
 }
-  /**
-   * Computes the factorial of the given value of `n`.
-   *
-   * @param n The value for which to compute the factorial.
-   * @return The factorial of n.
-   */
-  export function computeFactorial(n: number): number {
-    if (n === 0 || n === 1) {
-      return 1; // Base case: 0! and 1! both equal 1
-    }
-    return n * computeFactorial(n - 1);
-  }
-
-  /**
-   * Returns an array of the first `n` Fibonacci numbers starting from 1.
-   *
-   * @param n The first `n` of Fibonacci values to compute.
-   * @return An array containing the first `n` Fibonacci values.
-   */
-  export function getFirstNFibonacciNumbers(n: number): number[] {
-    if (n <= 0) {
-    return [];
-    }
-    else if (n === 1)  {
-      return [1];
-    }
-    return [];
-  } 
-  /**
-   * Finds a value in an array of values.
-   *
-   * @param values The values to search.
-   * @param start The left most index to search.
-   * @param end The right most index to search.
-   * @param value The value to look for.
-   * @return The index of the value if found in the array and -1 otherwise.
-   */
-  export function binarySearch(
-    values: number[],
-    start: number,
-    end: number,
-    value: number,
-  ): number {
-    if (end < start) {
-      // The range is not valid so just return -1.
-      return -1;
-    }
-
-    const pivotIndex = Math.floor((start + end) / 2); // The index in the middle of the array.
-    if (values[pivotIndex] === value) {
-      return pivotIndex;
-    } else if (values[pivotIndex] > value) {
-      return binarySearch(values, start, pivotIndex - 1, value);
-    } else {
-      return binarySearch(values, pivotIndex + 1, end, value);
-    }
-
-    // If values[pivotIndex] is equal to value then return `pivotIndex`.
-    // Else if values[pivotIndex] is greater than the value, then
-    // call `binarySearch(values, start, pivotIndex - 1, value)` and return its value;
-    // Else call `binarySearch(values, pivotIndex + 1, end, value)` and return its value.
-  }
