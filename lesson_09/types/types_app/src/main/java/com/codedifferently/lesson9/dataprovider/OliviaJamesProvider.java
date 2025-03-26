a54a1c9ea92813821dcf30ed19910c4ae23795a1
Olivia James <oxjames251@gmail.com>
2025-03-26 14:51:14 +0000

    :fixed: format

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/OliviaJamesProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/OliviaJamesProvider.java
@@ -1,12 +1,13 @@
 package com.codedifferently.lesson9.dataprovider;
 
 import java.util.Map;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class OliviaJamesProvider extends DataProvider {
+
   public String getProviderName() {
+
     return "oliviajames";
   }
 
