67899c891ce86791140058f17bf0f7ff1b6d44b5
Ezra Nyabuti <enyabuti251@gmail.com>
2025-03-27 17:04:13 +0000

    feat: adds Ezra Nyabuti's data loader

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { EzraNyabutiLoader } from './ezra_nyabuti_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
-const LOADER_PROVIDERS = [AnthonyMaysLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, EzraNyabutiLoader];
 
 @Module({
   providers: [
