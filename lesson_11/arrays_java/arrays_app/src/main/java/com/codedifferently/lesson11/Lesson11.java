e93f35c6f13b9c6375cf4f270ac11d9b6c556153
Dylan <dlafferty251@gmail.com>
2025-03-28 23:27:02 +0000

    Feat/Adds a getConcatenation method that doubles an arrays contents

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -8,8 +8,41 @@ public class Lesson11 {
    * Provide the solution to LeetCode 1929 here:
    * https://leetcode.com/problems/concatenation-of-array
    */
-  public int[] getConcatenation(int[] nums) {
-    return null;
+  public static int[] getConcatenation(int[] nums) {
+
+        int n = nums.length; //Creates a variable named n that is the length of nums
+        int[] ans = new int[2 * n]; //Creates a new array that is double the size of n.
+
+
+        for (int i = 0; i < n; ++i) {
+            ans[i] = nums[i]; //Gets the first half of the int array
+            ans[i + n] = nums[i]; //Gets the second half of the int array
+        }
+        return ans;
+    }
+
+ 
+    public static void main(String[] args) {
+         
+        //Creates an instance of the solution class
+        //We need this because getConcatenation() is a method that is non-static meaning we need and object of Solution to use it. 
+        //This would be unneeded if getConcatenation() was static
+
+
+        int[] nums = {1, 2, 3, 4}; //Array of nums based on Test input
+        int[] result = getConcatenation(nums); //calls the getConcatenation method giving it the array of nums.
+
+
+        //Prints result out.
+
+        //You need to loop out the result because we want to display the contents of an array which means we need to call each instance in it. This is easily done by using the for else loop to run until it is returned false.
+        for (int num: result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+
   }
 
   /**
