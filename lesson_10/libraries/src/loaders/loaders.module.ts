a5e02d7bd9b536a489ea14dbab0c3d200f13b936
Dadenaike251 <davidadenaike251@gmail.com>
2025-03-28 16:57:14 +0000

    feat:add DavidAdenaikeLoader Lesson_10HW

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -3,14 +3,16 @@ import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
+import { DavidAdenaikeLoader } from './david_adenaike_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
-const LOADER_PROVIDERS = [
+// const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
   DylanLaffertysLoader,
   MercedesMathewsLoader,
 ];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, DavidAdenaikeLoader];
 
 @Module({
   providers: [
