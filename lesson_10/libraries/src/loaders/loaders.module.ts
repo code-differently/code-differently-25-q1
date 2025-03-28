2a9c725dc738b7dab2a2ee1be47f10a67240a564
JEKLUND251 <jeklund251@gmail.com>
2025-03-28 11:40:08 -0400

    fix: fixing merge conflicts occurring in git.

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -3,11 +3,11 @@ import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { JustinEklundLoader } from './justin_eklund_loader.js';
-
+import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
-const LOADER_PROVIDERS = [AnthonyMaysLoader, DylanLaffertysLoader,JustinEklundLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, DylanLaffertysLoader,JustinEklundLoader, MercedesMathewsLoader];
 
 @Module({
   providers: [
