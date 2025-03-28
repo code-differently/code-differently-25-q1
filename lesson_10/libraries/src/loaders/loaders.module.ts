7a8e0ffd4787c7a9265a9c8659af3669b5ca0c22
kksaunders251 <kksaunders251@gmail.com>
2025-03-28 17:25:03 +0000

    Chore: resolving Errors in loaders.module.ts

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { KhaylaSaundersLoader } from './khayla_saunders_loader.js';
+import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
-const LOADER_PROVIDERS = [AnthonyMaysLoader, KhaylaSaundersLoader];
+const LOADER_PROVIDERS = [
+  AnthonyMaysLoader,
+  KhaylaSaundersLoader,
+  DylanLaffertysLoader,
+  MercedesMathewsLoader,
+];
 
 @Module({
   providers: [
