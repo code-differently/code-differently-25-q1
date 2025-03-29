1343d41ee25acca1f3760181cd015225e0b8827f
Evan Philakhong <evanthegamer2004@gmail.com>
2025-03-29 03:46:53 +0000

    feat: added lesson11.ts

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,7 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  let combinedArr = nums.concat(nums);
+  const combinedArr = nums.concat(nums);
   return combinedArr;
 }
 
@@ -12,7 +12,7 @@ export function getConcatenation(nums: number[]): number[] {
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  let result: number[] = [];
+  const result: number[] = [];
 
   for (let i = 0; i < words.length; i++) {
     if (words[i].includes(x)) {
@@ -20,5 +20,5 @@ export function findWordsContaining(words: string[], x: string): number[] {
     }
   }
 
-  return result; 
+  return result;
 }
