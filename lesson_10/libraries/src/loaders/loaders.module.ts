69547be41a01e196960ada4509b61acacf795db2
jxwatson251 <jxwatson251@gmail.com>
2025-03-30 00:15:46 +0000

    rebase lesson 10 homework

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { DylanLaffertysLoader } from './dylan_lafferty_loaders.js';
 import { MercedesMathewsLoader } from './mercedes_mathews_loader.js';
+import { MontezBLoader } from './montez_b_loaders.js';
+import { JasonWatsonLoader } from './jason_watson_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
@@ -10,6 +12,8 @@ const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
   DylanLaffertysLoader,
   MercedesMathewsLoader,
+  MontezBLoader,
+  JasonWatsonLoader
 ];
 
 @Module({
