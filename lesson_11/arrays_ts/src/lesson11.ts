1a12497f4669ce955414eda15084a3b3a967aa28
Meiko-S22 <mdstephens251@gmail,.com>
2025-03-31 15:09:01 +0000

    chore: adds extra credit

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,13 +3,28 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+   const answer: number[] = new Array(2 * nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        answer[i] = nums[i];
+        answer[i + nums.length] = nums[i];
+    }
+    return answer;
 }
+
 
 /**
  * Provide the solution to LeetCode 2942 here:
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+ const result: number[] = [];
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].indexOf(x) !== -1) {
+            result.push(i);
+        }
+    }
+
+    return result;
 }
+
