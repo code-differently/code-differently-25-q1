f3c896baea805e8c3b43d0a7a065843e0eb9ff42
karensprojects22 <koalabi251@gmail.com>
2025-04-01 19:30:33 +0000

    feat:adds Karens lesson11 leetcode solutions

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,5 +1,6 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
@@ -9,7 +10,18 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    // used AI to implement this solution
+    if (nums == null || nums.length == 0) {
+      return new int[0];
+    }
+    int originalLength = nums.length;
+    int[] result = new int[originalLength * 2];
+
+    for (int i = 0; i < originalLength; i++) {
+      result[i] = nums[i];
+      result[i + originalLength] = nums[i];
+    }
+    return result;
   }
 
   /**
@@ -17,6 +29,16 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    // Montell helped me out with with coding implementation
+    // need to create a string array
+    List<Integer> search = new ArrayList<>();
+    // iterate through elements in string array and compare them with the character
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].contains(String.valueOf(x))) {
+        search.add(i);
+      }
+    }
+    // return the index of the element in the string array that contains the character provided
+    return search;
   }
 }
