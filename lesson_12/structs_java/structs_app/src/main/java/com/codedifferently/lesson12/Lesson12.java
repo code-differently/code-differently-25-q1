946a92fcfe5579ebc03e00c76eed1585fd3c3fa3
Dylan <dlafferty251@gmail.com>
2025-04-01 23:54:43 +0000

    fix: improve code formatting and comments in Lesson12.java and Stack.java

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -6,24 +6,28 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public static String gameResult(ListNode head) {
-    int[] scoreBoard = {0, 0}; //Creates an array that will act as our scoreboard...hence the name
+    int[] scoreBoard = {0, 0}; // Creates an array that will act as our scoreboard...hence the name
 
-    while (head.next != null) { //Creates a loop that runs until the head Linked list is empty || null
-      if (head.val != head.next.val && head.val % 2 == 0) { //Checks if the current node's value is not equal to the node next to it.
-      //Also will check if the current not's value is even ( % 2 dividing by 2 to detemine)
-        if (head.val > head.next.val) {//Updates the array based on index.
-          scoreBoard[head.val % 2] += 1;  //If the head is divisible by 2 then update the first Even teams point
+    while (head.next
+        != null) { // Creates a loop that runs until the head Linked list is empty || null
+      if (head.val != head.next.val
+          && head.val % 2
+              == 0) { // Checks if the current node's value is not equal to the node next to it.
+        // Also will check if the current not's value is even ( % 2 dividing by 2 to detemine)
+        if (head.val > head.next.val) { // Updates the array based on index.
+          scoreBoard[head.val % 2] +=
+              1; // If the head is divisible by 2 then update the first Even teams point
         } else {
-          scoreBoard[head.next.val % 2] += 1; 
+          scoreBoard[head.next.val % 2] += 1;
         }
       }
       head = head.next; // Moves to the next Node
     }
 
     if (scoreBoard[0] == scoreBoard[1]) {
-      return "Tie"; //Returns tie if the scoreboard is even
+      return "Tie"; // Returns tie if the scoreboard is even
     } else if (scoreBoard[0] > scoreBoard[1]) {
-      return "Even"; //if the Even side [0] is greater than 1 return "Even"
+      return "Even"; // if the Even side [0] is greater than 1 return "Even"
     } else {
       return "Odd"; // Else return "Odd"
     }
