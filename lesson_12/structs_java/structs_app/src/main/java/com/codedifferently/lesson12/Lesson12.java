4b702713d06b20920e2bbf798f8bc1c6a987b49d
Dylan <dlafferty251@gmail.com>
2025-04-01 12:29:47 +0000

    Chore: Removes comment codefrom Lesson12.java

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -1,60 +1,31 @@
 package com.codedifferently.lesson12;
 
 public class Lesson12 {
-
-  // public static Node {
-  //   public int data;
-  //   public Node next;
-
-  //   public Node(int data) {
-  //     this.data = data;
-  //   }
-  // }
   /**
    * Provide the solution to LeetCode 3062 here:
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public static String gameResult(ListNode head) {
-    int[] scoreBoard = {0, 0};
+    int[] scoreBoard = {0, 0}; //Creates an array that will act as our scoreboard...hence the name
 
-    while (head.next != null) {
-      if (head.val != head.next.val && head.val % 2 == 0) {
-        if (head.val > head.next.val) {
-          scoreBoard[head.val % 2] += 1;
+    while (head.next != null) { //Creates a loop that runs until the head Linked list is empty || null
+      if (head.val != head.next.val && head.val % 2 == 0) { //Checks if the current node's value is not equal to the node next to it.
+      //Also will check if the current not's value is even ( % 2 dividing by 2 to detemine)
+        if (head.val > head.next.val) {//Updates the array based on index.
+          scoreBoard[head.val % 2] += 1;  //If the head is divisible by 2 then update the first Even teams point
         } else {
-          scoreBoard[head.next.val % 2] += 1;
+          scoreBoard[head.next.val % 2] += 1; 
         }
       }
       head = head.next; // Moves to the next Node
     }
 
     if (scoreBoard[0] == scoreBoard[1]) {
-      return "Tie";
+      return "Tie"; //Returns tie if the scoreboard is even
     } else if (scoreBoard[0] > scoreBoard[1]) {
-      return "Even";
+      return "Even"; //if the Even side [0] is greater than 1 return "Even"
     } else {
-      return "Odd";
+      return "Odd"; // Else return "Odd"
     }
   }
 }
-
-  // public static Node createLinkedList(int[] arr) {
-  //   if (arr.length == 0) return null;
-
-  //   Node head = new Node(arr[0]); //Creates the first node and names it head
-  //   Node current = head; //Tracks the current head inside the loop
-
-  //   for(int i = 1; i < arr.length; ++i) { //Starts a for loop that is at the second element
-  //     current.next = new Node(arr[i]); //creates a new node and links it
-  //     current = current.next;
-  //   }
-  //       return head;
-
-  // }
-
-  // public static void main(String[] args) {
-  //     int[] arr = {2, 5, 4, 7, 20, 5};
-
-  //     System.out.println(createLinkedList(arr));
-  // }
-// }
