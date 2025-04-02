6c5e88b84731d79b9d20a0657d22bffb0b339a6f
Dylan <dlafferty251@gmail.com>
2025-04-02 13:50:45 +0000

    Refactor: Removed Main method and test cases. Also made comments cleaning

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -5,14 +5,11 @@
 */
 export function getConcatenation(nums: number[]): number[] {
  const n = nums.length; //Creates a variable named n that is the length of nums
- const ans: number[] = new Array(2 * n); //Creates a new array that is double the size of n
-
+ const ans = new Array(2 * n); //Creates a new array that is double the size of n
 
  for(let i = 0; i < n; ++i) {
    ans[i] = nums[i];
    ans[i + n] = nums[i];
-
-
  }
  return ans;
 }
@@ -23,28 +20,14 @@ export function getConcatenation(nums: number[]): number[] {
 * https://leetcode.com/problems/find-words-containing-character/
 */
 export function findWordsContaining(words: string[], x: string): number[] {
- const elListo: number[] = [] //creates a new array named elListo
-
+ //creates a new array list names arrList
+ const arrList: number[] = [] 
 
  //Runs a loop that will iterate through each character in the String.
  for (let i = 0; i < words.length; ++i) {
    if (words[i].includes(x)) { //Checks to see if the word inclues the char 'x'
-     elListo.push(i); //Pushes the index of i if it is true that the word contains that character
-    
+     arrList.push(i); //Pushes the index of i if it is true that the word contains that character
    }
- } return elListo; //Will return the Array elListo
+ } return arrList; //Will return the Array elListo
 }
-
-
-//Test Cases
-
-
-const nums: number[] = [1, 2 , 3, 4]; //Array of numbers based on test case
-const result: number[] = getConcatenation(nums);
-console.log(result.join(' '))//Prints the test result out for the fet concatentaition method
-
-
-const words: string[] = ['leet', 'code', 'hello', 'world']; //Array of strings based on test case
-const x = 'e'; //Calls for a specific char
-console.log(findWordsContaining(words, x));
 
