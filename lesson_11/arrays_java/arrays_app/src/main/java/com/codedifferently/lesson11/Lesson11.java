343a75739b47ab7e2278901e6ebe4a09f8cc959d
JEKLUND251 <JEKLUND251@gmail.com>
2025-04-02 15:39:08 +0000

    feat: added work into lesson 11

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,22 +1,26 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
 
-  /**
-   * Provide the solution to LeetCode 1929 here:
-   * https://leetcode.com/problems/concatenation-of-array
-   */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int answer[] = new int[nums.length * 2];
+    for (int i = 0; i < nums.length; i++) {
+      answer[i] = nums[i];
+      answer[i + nums.length] = nums[i];
+    }
+    return answer;
   }
 
-  /**
-   * Provide the solution to LeetCode 2942 here:
-   * https://leetcode.com/problems/find-words-containing-character/
-   */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    var result = new ArrayList<Integer>();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].contains(String.valueOf(x))) {
+        result.add(i);
+      }
+    }
+    return result;
   }
 }
--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -1,15 +1,32 @@
-/**
- * Provide the solution to LeetCode 1929 here:
- * https://leetcode.com/problems/concatenation-of-array
- */
+
 export function getConcatenation(nums: number[]): number[] {
-  return [];
-}
+  if (nums.length === 0) {
+    return [];
+  }
+    const result = [...nums];
+    for (const num of nums) { 
+      result.push(num);
+    }
+    return result;
+  }
+
+  
+
 
 /**
  * Provide the solution to LeetCode 2942 here:
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
-}
+  if (words.length === 0) {
+    return [];
+  }
+    const result = [];
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].includes(x)) {
+        result.push(i);
+      }
+    }
+    return result;
+  }
+//Received help from copiliot and Meiko and Bryana. 
