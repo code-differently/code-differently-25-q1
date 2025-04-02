2ed323a0bd95aaaf5d9babe0db42c48c96349d69
“Tezz03” <mmbradley@gmail.com>
2025-04-02 17:05:30 +0000

    feat: mbLesson_12

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -10,17 +10,22 @@ public class Stack {
 
   public void push(int value) {
     // Your code here
+    ListNode newNode = new ListNode(value);
+    newNode.next = top;
+    top = newNode;
   }
 
   public int pop() {
-    return 0;
+    int result = top.val;
+    top = top.next;
+    return result;
   }
 
   public int peek() {
-    return 0;
+    return top.val;
   }
 
   public boolean isEmpty() {
-    return true;
+    return top == null;
   }
 }
