b4bab2d6d86f9778b7e00dc366daa7923618b39e
karensprojects22 <koalabi251@gmail.com>
2025-04-02 16:52:01 +0000

    feat: adds Karens lesson_12

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,44 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    
+    //team values
+    int oddValue = 0;
+    int evenValue = 0;
+
+    //iterate through listNode
+    //grabbed items
+    //accessed value
+
+    while (head != null && head.next != null) {
+      int val1 = head.val; //at head node, accessing value. head val is even
+      int val2 = head.next.val; // at next node, accessing value. next val is odd
+
+      //compare values
+      if (val1 > val2) {
+         evenValue += 1;
+      } else if (val1 < val2) {
+        oddValue += 1;
+      }
+      // go to next value
+        
+    }
+    
+
+
+
+
+
+
+    // loop through lstNode
+    
+    // grab lstNode values
+    //compare them
+    //return even or odd depending on which value is higher
+
+  
+  
+
   }
+
 }
