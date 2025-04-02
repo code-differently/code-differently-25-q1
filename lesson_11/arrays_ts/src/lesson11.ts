12b44b2176a60738c80e18336f3f5421e72a2269
Meiko-S22 <mdstephens251@gmail,.com>
2025-04-02 13:05:40 +0000

    fix: formatting for extra credit

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,7 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-   const answer: number[] = new Array(2 * nums.length);
+    const answer: number[] = new Array(2 * nums.length);
     for (let i = 0; i < nums.length; i++) {
         answer[i] = nums[i];
         answer[i + nums.length] = nums[i];
@@ -17,14 +17,13 @@ export function getConcatenation(nums: number[]): number[] {
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
- const result: number[] = [];
 
+    const result: number[] = [];
     for (let i = 0; i < words.length; i++) {
         if (words[i].indexOf(x) !== -1) {
             result.push(i);
         }
     }
-
     return result;
 }
 
