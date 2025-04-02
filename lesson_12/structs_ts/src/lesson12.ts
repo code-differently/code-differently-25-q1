4572aa415e048c50c5c23de73a410c63418978f0
Rasheed <rmillercohort251@gmail.com>
2025-04-02 15:00:10 +0000

    Implemented the gameResult and stack methods

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -1,11 +1,29 @@
 import { ListNode } from './list_node.js';
 
 export class Lesson12 {
-  /**
-   * Provide the solution to LeetCode 3062 here:
-   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
-   */
+  
   public gameResult(head: ListNode | null): string {
-    return '';
-  }
+     if (!head) return 'Tie';
+    
+    const ls = [0, 0];
+    
+    while (head.next) {
+        if (head.val !== head.next.val && head.val % 2 === 0) {
+            if (head.val > head.next.val) {
+                ls[head.val % 2] += 1;
+            } else {
+                ls[head.next.val % 2] += 1;
+            }
+        }
+        head = head.next;
+    }
+    
+    if (ls[0] === ls[1]) {
+        return 'Tie';
+    } else if (ls[0] > ls[1]) {
+        return 'Even';
+    } else {
+        return 'Odd';
+    }
+}
 }
