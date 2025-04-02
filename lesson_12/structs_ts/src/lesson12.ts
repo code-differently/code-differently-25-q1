3bc3669e55ed90633a90c37c795085a38d4f6534
mercedes-mathews <memathews251@gmail.com>
2025-04-02 01:50:31 +0000

    chore: implemented functions in the lesson12.ts and stack.ts files

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -6,6 +6,25 @@ export class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public gameResult(head: ListNode | null): string {
-    return '';
+    let even = 0;
+    let odd = 0;
+    let evenNode: ListNode | null = head;
+    let oddNode: ListNode | null = head?.next ?? null;
+    while (evenNode != null && oddNode != null) {
+      if (evenNode.val > oddNode.val) {
+        even++;
+      } else {
+        odd++;
+      }
+      evenNode = evenNode.next?.next ?? null;
+      oddNode = oddNode.next?.next ?? null;
+    }
+    if (even > odd) {
+      return 'Even';
+    } else if (odd > even) {
+      return 'Odd';
+    } else {
+      return 'Tie';
+    }
   }
 }
--- lesson_12/structs_ts/src/stack.ts
+++ lesson_12/structs_ts/src/stack.ts
@@ -8,18 +8,24 @@ export class Stack {
   }
 
   push(value: number): void {
-    throw new Error('Not implemented');
+    const newNode = new ListNode(value, this.top);
+    this.top = newNode;
   }
 
   pop(): number | undefined {
-    throw new Error('Not implemented');
+    const value = this.top?.val;
+    this.top = this.top?.next;
+    return value;
   }
 
   peek(): number | null {
-    throw new Error('Not implemented');
+    if (!this.top) {
+      throw new Error('Stack is empty');
+    }
+    return this.top.val;
   }
 
   isEmpty(): boolean {
-    throw new Error('Not implemented');
+    return this.top == null;
   }
 }
