60bd569a4fe0a0c08c0eb2ec60c4ee1a2c162bdc
jxwatson251 <jxwatson251@gmail.com>
2025-04-02 14:04:50 +0000

    lesson_12_homework_structs_ts

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,24 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int[] scores = {0, 0};
+    while (head != null && head.next != null) {
+      if (head.val != head.next.val && head.val % 2 == 0) {
+        if (head.val > head.next.val) {
+          scores[head.val % 2] += 1;
+        } else {
+          scores[head.next.val % 2] += 1;
+        }
+      }
+      head = head.next;
+    }
+
+    if (scores[0] == scores[1]) {
+      return "Tie";
+    } else if (scores[0] > scores[1]) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
   }
 }
