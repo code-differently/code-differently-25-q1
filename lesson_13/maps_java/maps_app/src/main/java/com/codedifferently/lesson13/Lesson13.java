5fef93bb224b839334c311ce2fe9c53244603cc9
Dylan <dlafferty251@gmail.com>
2025-04-03 00:35:02 +0000

    feat: implement findPermutationDifference method in Lesson13 class

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +9,27 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    var charByIndex = new HashMap<Character, Integer>();
+
+    // Stores each character index from s into an Array
+    for (int i = 0; i < s.length(); ++i) {
+      charByIndex.put(s.charAt(i), i);
+    }
+
+    // will be the difference between s and t's index's
+    int difference = 0;
+
+    for (int j = 0; j < t.length(); ++j) {
+      if (charByIndex.containsKey(t.charAt(j))) {
+        // Math.abs gets the absole number ensuring the difference is always positive
+        // My logic here is that it will find the index of t and subtract that from j which is that
+        // original index of that char
+        difference += Math.abs(charByIndex.get(t.charAt(j)) - j);
+      }
+    }
+    // Runs a for loop that will be the length of t but this time we will be using the map t
+    // tell our loop to run until it finds the same character
+    System.out.println(charByIndex);
+    return difference;
   }
 }
