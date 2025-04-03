2a47512294d1da1b0a2e03c1d746e176c7854550
JEKLUND251 <JEKLUND251@gmail.com>
2025-04-03 20:25:23 +0000

    task:added lesson 13 work into branch

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,12 +1,23 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
-  /**
-   * Provide the solution to LeetCode 3146 here:
-   * https://leetcode.com/problems/permutation-difference-between-two-strings
-   */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+
+    HashMap<Character, Integer> indexMap = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      indexMap.put(s.charAt(i), i);
+    }
+    // calculating the difference
+    int difference = 0;
+    for (int i = 0; i < t.length(); i++) {
+      if (indexMap.containsKey(t.charAt(i))) {
+        difference += Math.abs(indexMap.get(t.charAt(i)) - i);
+      }
+    }
+    return difference;
   }
 }
+// I got help from chat gpt and co pilot. I used chat gpt to further explain the code that I was putting in.
