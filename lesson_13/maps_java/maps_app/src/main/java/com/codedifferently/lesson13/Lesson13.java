e0c0bb6a0ec4f8e1edede97a99875f24a91b11e4
Meiko-S22 <mdstephens251@gmail,.com>
2025-04-03 20:23:35 +0000

    feat: adds Meiko's lesson 13

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -9,14 +9,14 @@ public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
     int ans = 0;
     int[] indices = new int[26];
-    
-    for (int i = 0; i < s.length(); ++i){
-      indices[s.charAt(i) - 'a'] = i;
-    } 
 
-    for (int i = 0; i < t.length(); ++i){
+    for (int i = 0; i < s.length(); ++i) {
+      indices[s.charAt(i) - 'a'] = i;
+    }
+
+    for (int i = 0; i < t.length(); ++i) {
       ans += Math.abs(indices[t.charAt(i) - 'a'] - i);
-    } 
+    }
 
     return ans;
   }
