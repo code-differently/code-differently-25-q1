aa5ca180a824f0611034bd3602a8f39dac162126
Meiko-S22 <mdstephens251@gmail,.com>
2025-04-03 20:16:08 +0000

    feat: adds Meiko's lesson 13

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -7,6 +7,17 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    int ans = 0;
+    int[] indices = new int[26];
+    
+    for (int i = 0; i < s.length(); ++i){
+      indices[s.charAt(i) - 'a'] = i;
+    } 
+
+    for (int i = 0; i < t.length(); ++i){
+      ans += Math.abs(indices[t.charAt(i) - 'a'] - i);
+    } 
+
+    return ans;
   }
 }
