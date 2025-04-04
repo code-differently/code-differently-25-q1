e610370577018f18458f8d40c3c55406914cf240
karensprojects22 <koalabi251@gmail.com>
2025-04-04 15:34:42 +0000

    feat: adds lesson 13 permutation difference solution in typescript

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,26 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  // used chat gpt for this solution implementation
+    
+  // variable difference keeps track of absolute difference between character index
+  let difference = 0;
+    
+    // creating a map to store the index of each character in s
+    const positionMap: Record<string, number> = {};
+    // iterating through the characters in s with i as the index
+    for (let i = 0; i < s.length; i++) {
+      // returns the character at a given index 
+        positionMap[s[i]] = i;
+    }
+    
+    // calculating permutation difference
+    for (let i = 0; i < t.length; i++) {
+      //using a built in function - math.abs to calculate the difference in character index
+      // index of character in string s - index of character in string i
+        difference += Math.abs(i - positionMap[t[i]]);
+    }
+    
+    return difference;
 }
+
