f476c5333b81bbf7d61c016320b5903f0be2d289
kksaunders251 <kksaunders251@gmail.com>
2025-04-04 16:42:54 +0000

    Feat: lesson 13 leet code java solution

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +9,30 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    HashMap<Character, Integer> indexMap = new HashMap <> ();
+    for (int i=0; i< s.length(); i++){
+  indexMap.put(s.charAt(i), i);
+}
+int difference =0;
+
+for (int i =0; i < t.length(); i++) {
+  char ch = t.charAt(i);
+  int originalIndex = indexMap.get (ch); 
+  difference += Math.abs(originalIndex -i);
+
+}
+return difference;
+  }
+  //used chatgpt because I needed help creating an instance before calling 
+   public static void main(String[] args) {
+    // Create an object of Lesson13 to call the method
+    Lesson13 obj = new Lesson13();
+    System.out.println(obj.findPermutationDifference("abc", "bac"));  // Output: 2
+    System.out.println(obj.findPermutationDifference("abcde", "edbac"));  // Output: 12
   }
 }
+//Constraints:
+//1 <= s.length <= 26
+//Each character occurs at most once in s.
+//t is a permutation of s.
+//s consists only of lowercase English letters.
