2fb9ff2c79bf328afdd2cb8eb1ed5e50ee52a162
NelltheWiz <chutt251@gmail.com>
2025-04-04 15:49:58 +0000

    feat: completes lesson 13

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,11 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  let difference = 0;
+  for (let i = 0; i < s.length; i++) {
+    //for loop is to itirate through the string
+    difference += Math.abs(i - t.indexOf(s[i])); // Add absolute differences using an Indexof function
+    // Co pilot fixed my error. I had charAt originally it would not pass
+  }
+  return difference;
 }
