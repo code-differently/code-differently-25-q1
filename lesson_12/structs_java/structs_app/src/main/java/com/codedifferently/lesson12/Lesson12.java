5fdf6cef5a85a01b271fb9c42191d38487c1ae47
Ananatawa <nanamcintyre251@gmail.com>
2025-04-06 23:18:32 +0000

    feat: Adds Nana's Lesson12.java and Stack.java

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,30 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int evenTeam = 0;
+    int oddTeam = 0;
+
+    while (head !=null && head.next != null) {
+      int current = head.val;
+      int next = head.next.val;
+
+      if (current != next && current %2 == 0) {
+        if (current > next) {
+          evenTeam++;
+        } else {
+          oddTeam++;
+        }
+      }
+  
+      head = head.next;
+    }
+
+    if (evenTeam == oddTeam) {
+      return "Tie";
+    } else if (evenTeam > oddTeam) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
   }
 }
--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -9,18 +9,37 @@ public class Stack {
   }
 
   public void push(int value) {
-    // Your code here
+  ListNode newNode = new ListNode(value);
+    newNode.next = top;
+    top = newNode;
   }
 
   public int pop() {
-    return 0;
+   if (isEmpty()) {
+      throw new RuntimeException("Stack Underflow: Can't pop from an empty stack");
+    }
+    int poppedValue = top.val;
+    top = top.next;
+    return poppedValue;
   }
 
   public int peek() {
-    return 0;
+     if (isEmpty()) {
+      throw new RuntimeException("Stack is empty: Cannot peek");
+    }
+    return top.val;
   }
 
   public boolean isEmpty() {
-    return true;
+    return top == null;
   }
+
+  public void printStack() {
+    ListNode current = top;
+    while (current != null) {
+      System.out.print(current.val + " -> ");
+      current = current.next;
+    }
+    System.out.println("null");
+  } 
 }
