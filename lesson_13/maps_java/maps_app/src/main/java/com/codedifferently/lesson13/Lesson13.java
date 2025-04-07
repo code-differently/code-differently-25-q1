a551f55077874976f349a0a2efbaff4d4e19df9c
Olivia James <oxjames251@gmail.com>
2025-04-07 17:42:33 +0000

    feat:add lesson 13

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +9,18 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    HashMap<Character, Integer> indexMap = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      indexMap.put(s.charAt(i), i);
+    }
+    int difference = 0;
+    for (int i = 0; i < t.length(); i++) {
+      if (indexMap.containsKey(t.charAt(i))) {
+        difference += Math.abs(indexMap.get(t.charAt(i)) - i);
+      }
+    }
+    return difference;
   }
 }
+// Used ChatGPT to explain code.
+ 
