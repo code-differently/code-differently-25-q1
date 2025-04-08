c3e1b5d5e70d7b87b9e91679d19a0697c2360a66
Davis Darius <dwdavis251@gmail.com>
2025-04-08 13:28:44 +0000

    feat:add Davis stack and leetcode prompt (3062)HW

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,26 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int[] ls = new int[2];
+    while (head != null && head.next != null) {
+      // Iterate through the linked list until the second last node
+      if (head.val != head.next.val && head.val % 2 == 0) {
+        // Check if we have different values and if the current value is even
+        if (head.val > head.next.val) {
+          ls[head.val % 2] += 1;
+        } else {
+          ls[head.next.val % 2] += 1;
+        }
+      }
+      head = head.next;
+    }
+
+    if (ls[0] > ls[1]) {
+      return "Even";
+    } else if (ls[0] == ls[1]) {
+      return "Tie";
+    } else {
+      return "Odd";
+    }
   }
 }
