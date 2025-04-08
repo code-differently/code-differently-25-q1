a59cefec83fbef0d82a05728e1be674fa99f624f
kksaunders251 <kksaunders251@gmail.com>
2025-04-08 13:20:13 +0000

    feat: Adds tests for Employee setters and EmployeeManager addEmployee method

---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15TestManager.java
@@ -0,0 +1,12 @@
+package com.codedifferently.lesson15;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class Lesson15Test {
+    @Test
+    public void addEmployee (){
+        assertThat(new )
+    }
+}
