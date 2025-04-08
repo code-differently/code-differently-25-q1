d4b0f8e0ddf8b01d16ca8bb6f5d28ad3ac4f0b80
kksaunders251 <kksaunders251@gmail.com>
2025-04-08 15:02:22 +0000

    feat: adds Employee unit test

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
@@ -1,7 +1,7 @@
 package com.codedifferently.lesson15;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class Lesson15Test {
@@ -16,92 +16,4 @@ class Lesson15Test {
     // Act
     Lesson15.main(null);
   }
-      @Test
-  public void getEmployeeId () {
-    // Arrange
-    var employeeId = 4;
-
-    //Act
-Employee myEmployee = new Employee(employeeId, "", "", 0);
-
-    //Assert
-    assertEquals(4, myEmployee.getId());
-}
-    @Test
-  public void setEmployeeId () {
-    // Arrange
-    var expectedId = 5;
-
-    //Act
-Employee setEmployee = new Employee(5, "", "", 0);
-
-    //Assert
-   assertEquals(expectedId, setEmployee.getId());
-}
-  @Test
-  public void getName () {
-    // Arrange
-    var expectedName = "Khai";
-
-    //Act
-Employee nameEmployee = new Employee (0, "Khai", "", 0);
-
-    //Assert
-   assertEquals(expectedName, nameEmployee.getName());
-}
-  @Test
-  public void setName () {
-    // Arrange
-    var getName = "Khai";
-
-    //Act
-    Employee setEmployeeName = new Employee (0, "Khai", "", 0);
-
-    //Assert
-    assertEquals(getName, setEmployeeName.getName());
-}
- @Test
-  public void getDepartment() {
-    // Arrange
-    var myDepartment = "Tech";
-
-    //Act
-Employee employeeDepartment = new Employee(0, "", "Tech", 0);
-
-    //Assert
-    assertEquals(myDepartment, employeeDepartment.getDepartment());
-}
-   @Test
-  public void setDepartment() {
-    // Arrange
-    var expectedDepartment = "Software";
-
-    //Act
-Employee placeDepartment = new Employee(0, "", "Software", 0);
-
-    //Assert
-    assertEquals(expectedDepartment, placeDepartment.getDepartment()); 
-}
- @Test
-  public void getSalary() {
-    // Arrange
-    var mySalary = 1111;
-
-    //Act
-Employee expectedSalary = new Employee(0, "", "", 1111);
-
-    //Assert
-    assertEquals(mySalary, expectedSalary.getSalary()); 
-}
-@Test
-  public void setSalary() {
-    // Arrange
-    var getSalary = 1111;
-
-    //Act
-Employee employeeSalary = new Employee(0, "", "", 1111);
-
-    //Assert
-    assertEquals(getSalary, employeeSalary.getSalary()); 
-}
 }
--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15TestManager.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15TestManager.java
@@ -1,12 +1,23 @@
 package com.codedifferently.lesson15;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Lesson15Test {
-    @Test
-    public void addEmployee (){
-        assertThat(new )
-    }
+public class Lesson15TestManager {
+
+  EmployeeManager manager;
+
+  @BeforeEach
+  void setUp() {
+    manager = new EmployeeManager();
+  }
+
+  @Test
+  void addEmployee_addsEmployeeToMap() {
+    Employee newEmployee = new Employee(1, "K", "Tech", 1);
+    manager.addEmployee(newEmployee);
+    assertThat(manager.getEmployee(1)).isEqualTo(newEmployee);
+  }
 }
