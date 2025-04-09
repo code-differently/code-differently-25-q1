6ffda9d9669ad3388ed7bfb2e7db36f00677e06d
Liv <oxjames251@gmail.com>
2025-04-09 12:03:23 -0400

    Revert Lesson15Test.java Changes

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
@@ -1,6 +1,7 @@
 package com.codedifferently.lesson15;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class Lesson15Test {
@@ -15,21 +16,4 @@ class Lesson15Test {
     // Act
     Lesson15.main(null);
   }
-  @Test
-  public void testGetId(){
-    Employee employee = new Employee(1, "Olivia James", "IT", 100000);
-    int id = employee.getId();
-    assertThat(id).isEqualTo(1);
-
-  }
-@Test
-public void testSetId (){
-  Employee employee = new Employee(1, "Olivia James", "IT", 100000);
-  employee.setId(2);
-   assertThat(employee.getId()).isEqualTo(2);
-}
-
-
-
-
 }
