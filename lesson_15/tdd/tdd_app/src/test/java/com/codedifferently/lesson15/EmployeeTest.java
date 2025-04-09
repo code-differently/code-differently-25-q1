28e08bdf0d616bd25bf1119e7ec22a73d6f8dbe9
Olivia James <oxjames251@gmail.com>
2025-04-09 15:58:31 +0000

    feat:add employee tests

---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeTest.java
@@ -0,0 +1,61 @@
+package com.codedifferently.lesson15;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+  @Test
+  public void testGetId(){
+    Employee employee = new Employee(1, "Olivia James", "IT", 100000);
+    int id = employee.getId();
+    assertThat(id).isEqualTo(1);
+
+  }
+@Test
+public void testSetId (){
+  Employee employee = new Employee(1, "Olivia James", "IT", 100000);
+  employee.setId(2);
+   assertThat(employee.getId()).isEqualTo(2);
+}
+
+@Test 
+public void testGetname (){
+  Employee employee = new Employee(1, "Olivia James", "IT", 100000);
+  assertThat(employee.getName()).isEqualTo("Olivia James");
+}
+
+@Test
+public void testSetname (){
+  Employee employee = new Employee(1, "Olivia James", "IT", 100000);
+  employee.setName("Vicente");
+  assertThat(employee.getName().equals("Vicente"));
+}
+
+@Test
+public void getDepartment(){
+    Employee employee= new Employee(1, "Olivia James", "IT", 100000);
+     assertThat(employee.getDepartment()).isEqualTo("IT");
+}
+
+@Test
+public void setDepartment(){
+    Employee employee= new Employee(1, "Olivia James", "IT", 100000);
+    employee.setDepartment("HR");
+     assertThat(employee.getDepartment().equals("HR"));
+}
+
+@Test
+public void getSalary(){
+    Employee employee= new Employee(1, "Olivia James", "IT", 100000);
+     assertThat(employee.getSalary()).isEqualTo(100000);
+}
+
+@Test
+public void setSalary(){
+    Employee employee= new Employee(1, "Olivia James", "IT", 100000);
+    employee.setSalary(50000);
+    assertThat(employee.getSalary()).isEqualTo(50000);
+}
+}
+
+
