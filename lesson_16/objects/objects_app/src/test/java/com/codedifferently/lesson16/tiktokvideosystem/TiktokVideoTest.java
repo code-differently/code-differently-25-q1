76707f4e841cc1ef78010e40b5353e92ec0f2c7e
kksaunders251 <kksaunders251@gmail.com>
2025-04-11 14:01:31 +0000

    feat: adds unit test  to test file and updates formatting in exception & main files

--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/tiktokvideosystem/TiktokVideoTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/tiktokvideosystem/TiktokVideoTest.java
@@ -1,10 +1,49 @@
 package com.codedifferently.lesson16.tiktokvideosystem;
-import static com.codedifferently.lesson16.tiktokvideosystem.InvalidViewIncrementException;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.hamcrest.MatcherAssert.assertThat.assertEquals;
+import org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TiktokVideoTest {
-    @Test 
-    
+
+  private TiktokVideo tiktokVideo;
+
+  @BeforeEach
+  void setUp() {
+    tiktokVideo =
+        new TiktokVideo("enigivensunday", 1000000, 500000, TiktokVideo.VideoCategory.VLOG);
+  }
+
+  @Test
+  void testConstructor() {
+    TiktokVideo video = new TiktokVideo("Jane", 3000, 1000, VideoCategory.DANCE);
+  }
+
+  @Test
+  void getVideoCategory() {
+    assertEquals(TiktokVideo.VideoCategory.VLOG, tiktokVideo.getVideoCategory());
+  }
+
+  @Test
+  void testIncreaseViewsPositive() {
+    tiktokVideo.increaseViews(500);
+    // got the is matcher idea from chat gpt
+    assertThat(tiktokVideo.getViewsCount(), is(1500));
+  }
+
+  @Test
+  void testDecreaseViewsNegative() {
+    tiktokVideo.InvalidViewIncrementException(-78);
+    assertThrows(InvalidViewIncrementException.class, () -> tiktokVideo);
+  }
+
+  @Test
+  String showCommennts() {}
+
+  @Test
+  void getCreatorName() {
+
+    assertEquals(TiktokVideo.String, tiktokVideo.getCreator());
+  }
 }
