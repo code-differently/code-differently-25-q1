4d25713b455ba4110122082aae08f3f800eecdff
Dylan <dlafferty251@gmail.com>
2025-04-15 12:58:52 +0000

    Chore: Adds JavaDoc to AuditLog and SavingsAccount

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/AuditLog.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/AuditLog.java
@@ -5,15 +5,40 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * The {@code AuditLog} class is responsible for logging important events in the banking system. It
+ * uses the Log4j logging framework to log messages at different levels.
+ */
 public class AuditLog {
+
   private static final Logger logger = LogManager.getLogger(AuditLog.class.getName());
   private final List<String> logEntries = new ArrayList<>();
 
+  /**
+   * Logs a message at the INFO level and adds it to the internal list of log entries.
+   *
+   * @param message
+   */
   public void log(String message) {
     logger.info(message);
     logEntries.add(message);
   }
 
+  /**
+   * Logs an error message at the ERROR level and adds it to the internal list of log entries.
+   *
+   * @param message
+   */
+  public void logError(String message) {
+    logger.error(message);
+    logEntries.add(message);
+  }
+
+  /**
+   * Returns the list of log entries.
+   *
+   * @return A list of log entries
+   */
   public List<String> getLogEntries() {
     return logEntries;
   }
--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/SavingsAccount.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/SavingsAccount.java
@@ -2,17 +2,39 @@ package com.codedifferently.lesson17.bank;
 
 import java.util.Set;
 
+/**
+ * The {@code SavingsAccount} class represents a savings account in a banking system. It extends the
+ * {@code CheckingAccount} class and provides additional functionality specific to savings accounts.
+ */
 public class SavingsAccount extends CheckingAccount {
 
+  /**
+   * Constructs a new SavingsAccount with the specified account number, owners, and balance.
+   *
+   * @param accountNumber
+   * @param owners
+   * @param balance
+   */
   public SavingsAccount(String accountNumber, Set<Customer> owners, double balance) {
     super(accountNumber, owners, balance);
   }
 
+  /**
+   * Gets the HashCode of the account number.
+   *
+   * @return The hash code of the account number.
+   */
   @Override
   public int hashCode() {
     return getAccountNumber().hashCode();
   }
 
+  /**
+   * Checks if this SavingsAccount is equal to another object.
+   *
+   * @param obj The object to compare with.
+   * @return true if the objects are equal, false otherwise.
+   */
   @Override
   public boolean equals(Object obj) {
     if (obj instanceof SavingsAccount other) {
@@ -24,11 +46,22 @@ public class SavingsAccount extends CheckingAccount {
     return false;
   }
 
+  /**
+   * Will throw and exception if the user attempt to make a withdrawl while under the instance of
+   * SavingsAccount.
+   *
+   * @param amount The amount to deposit.
+   */
   @Override
   public void withdraw(double amount) {
     throw new RuntimeException("Cannot withdraw from a savings account using a check");
   }
 
+  /**
+   * Returns a string representation of the SavingsAccount.
+   *
+   * @return A string representation of the SavingsAccount.
+   */
   @Override
   public String toString() {
     return "SavingsAccount"
