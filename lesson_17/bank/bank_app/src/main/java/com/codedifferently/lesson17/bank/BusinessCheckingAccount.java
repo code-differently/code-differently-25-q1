3cae5548ae964e56b1bc24c68bc274aa8e641b8e
jxwatson251 <jxwatson251@gmail.com>
2025-04-15 18:51:29 +0000

    lesson 17 homework mark3

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
@@ -2,8 +2,8 @@ package com.codedifferently.lesson17.bank;
 
 import java.util.Set;
 
-public class BusinessCheckingSavingsAccount extends CheckingAccount {
-  public BusinessCheckingSavingsAccount(
+public class BusinessCheckingAccount extends CheckingAccount {
+  public BusinessCheckingAccount(
       String accountNumber, Set<Customer> owners, double initialBalance) {
     super(accountNumber, owners, initialBalance);
   }
--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/SavingsAccount.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/SavingsAccount.java
@@ -2,18 +2,8 @@ package com.codedifferently.lesson17.bank;
 
 import java.util.Set;
 
-public class SavingsAccount extends CheckingAccount {
+public class SavingsAccount extends Account {
   public SavingsAccount(String accountNumber, Set<Customer> owners, double initialBalance) {
     super(accountNumber, owners, initialBalance);
-  }
-
-  @Override
-  public void withdraw(double amount) {
-    String method = null;
-    if ("check".equalsIgnoreCase(method)) {
-      throw new UnsupportedOperationException("Cannot withdraw checks from the savings account");
-    }
-
-    this.balance += amount;
   }
 }
