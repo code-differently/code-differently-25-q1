fe5afadb1257a7716bb0880d96f9a24ad693df15
bryanasb251 <bsingletonbarnhart251@gmail.com>
2025-04-16 13:29:19 +0000

    refactor: clean up CheckTest by removing commented-out tests and unused account instance

--- lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/CheckTest.java
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/CheckTest.java
@@ -9,36 +9,14 @@ import org.junit.jupiter.api.Test;
 class CheckTest {
 
   private CheckingAccount account1;
-  private CheckingAccount account2;
   private Check classUnderTest;
 
   @BeforeEach
   void setUp() {
     account1 = new CheckingAccount("123456789", null, 100.0);
-    account2 = new CheckingAccount("987654321", null, 200.0);
+
     classUnderTest = new Check("123456789", 50.0, account1);
   }
-
-  // @Test
-  // void testDepositFunds() {
-  //   // Act
-  //   classUnderTest.depositFunds(account2);
-
-  //   // Assert
-  //   assertThat(account1.getBalance()).isEqualTo(50.0);
-  //   assertThat(account2.getBalance()).isEqualTo(250.0);
-  // }
-
-  // @Test
-  // void testDepositFunds_CheckVoided() {
-  //   // Arrange
-  //   classUnderTest.voidCheck();
-
-  //   // Act & Assert
-  //   assertThatExceptionOfType(CheckVoidedException.class)
-  //       .isThrownBy(() -> classUnderTest.depositFunds(account2))
-  //       .withMessage("Check is voided");
-  // }
 
   @Test
   void testConstructor_CantCreateCheckWithNegativeAmount() {
@@ -74,16 +52,4 @@ class CheckTest {
     assertThat(classUnderTest.toString())
         .isEqualTo("Check{checkNumber='123456789', amount=50.0, account=123456789}");
   }
-
-  /* @Test
-  void testDepositFunds_DoesntDepositCheckTwice() {
-    Check check = new Check("987654321", 100.0, account1);
-
-    classUnderTest.depositFunds(account2);
-
-
-    assertThatExceptionOfType(CheckVoidedException.class)
-        .isThrownBy(() -> classUnderTest.depositFunds(account2))
-        .withMessage("Check is voided");
-  }*/
 }
