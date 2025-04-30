1893189e6cb58d2f9d369e30447ca3c9a64570d5
Dylan <dlafferty251@gmail.com>
2025-04-30 00:08:43 +0000

    feat: takes form submission from contact page and sends you to a new page

--- lesson_24/dylanlafferty/index.js
+++ lesson_24/dylanlafferty/index.js
@@ -6,9 +6,11 @@ var debug = require('debug')('dylanlafferty:server')
 const app = express()
 
 app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views")); 
 
 app.use(morgan("dev")) //Sets morgan middleware
 app.use(express.static(path.join(__dirname,"public"))) //this will get the files from public
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
 
@@ -27,7 +29,7 @@ const contactRouter = require('./routes/contact-us') //calls for my router
 app.use("/contact-us", contactRouter)
 
 app.post("/", (req, res) => {
-    res.render("public")
+    res.render("")
 })
 
 app.listen(PORT, () => {
--- lesson_24/dylanlafferty/routes/contact-us.js
+++ lesson_24/dylanlafferty/routes/contact-us.js
@@ -1,16 +1,49 @@
 const express = require('express')
 const router = express.Router()
+const contacts = [{ name: "Dylan", email: "123dylan@gmail.com"}, {name: "John", email: "123john@gmail.com"}]
+
 
 router.get('/', (req, res) => {
-    res.render("contact.ejs")
-})
-
-router.get('/new', (req, res) => {
-    res.send("Thank you for submitting your contact info!")
+    res.render("contact")
 })
 
 router.post("/", (req, res) => {
-    res.render("contact")
+
+    const isValid = true
+    if(isValid) {
+        contacts.push({
+            firstname: req.body.firstname,
+            email: req.body.email
+
+        })
+
+        res.redirect(`/contact-us/${contacts.length - 1}`)
+    } else {
+        console.log("Error")
+        res.render('contact-us', {firstname: req.body.firstname})
+    }
+
+    console.log(req.body.firstname)
+
+
+})
+
+router
+    .route('/:id')
+        .get((req, res) => {
+            console.log(req.contact)
+            res.render("newcontact", { contact: req.contact })
+        }).put((req, res) => {
+            res.send(`Set user with ID ${req.params.id}`)
+        }).delete((req, res) => {
+            res.send(`Goodbye user with ID ${req.params.id}`)
+        })
+
+
+
+router.param('id', (req, res, next, id) => {
+    req.contact = contacts[id]
+    next()
 })
 
 module.exports = router
