11af671f6fc99ff2c751790973345b739ee66555
mercedes-mathews <memathews251@gmail.com>
2025-05-02 14:12:02 +0000

    feat: adds Program and ProgramList as components

---
+++ lesson_25/mercedesmathews/src/components/program/Program.tsx
@@ -0,0 +1,15 @@
+import './Program.scss';
+
+interface programProps {
+  header: string;
+  content: string;
+}
+
+export const Program = ({header, content}: programProps) => {
+  return (
+    <>
+      <h3>{header}</h3>
+      <p>{content}</p>
+    </>
+  );
+};
