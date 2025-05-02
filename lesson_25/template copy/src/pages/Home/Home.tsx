1ff6583f488f25d382a840831e65c408780e8f4f
Tezz03 <mmbradley251@gmail.com>
2025-05-02 15:45:13 +0000

    feat:upload program and programList to project

---
+++ lesson_25/template copy/src/pages/Home/Home.tsx
@@ -0,0 +1,31 @@
+import './Home.scss';
+import {Program} from '../components/program';
+import {programLists} from '../components/programList';
+import React from 'react';
+
+
+
+
+
+export const Home: React.FC = () => {
+  return (
+    <article>
+      <section className="hero-section">
+        <div className="hero-overlay"></div>
+        <div className="hero-content">
+          <h2 className="hero-title">
+            Together we can move the needle of{' '}
+            <em className="highlight">diversity in tech.</em>
+          </h2>
+          <div className="hero-text">
+            <span>Code Differently</span> provides hands on training and
+            education through coding classes that gives participants the
+            technical and cognitive skills they need to excel in
+            technology-driven workplaces.
+          </div>
+        </div>
+      </section>
+      
+    </article>
+  );
+};
