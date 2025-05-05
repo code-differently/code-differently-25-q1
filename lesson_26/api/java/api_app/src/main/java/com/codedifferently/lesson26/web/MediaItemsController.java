fc73f23c41d0a49a401ca757541d4f28633471ab
Tezz03 <mmbradley251@gmail.com>
2025-05-05 16:58:33 +0000

    fix: fix debug in mediaitemsController

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -6,7 +6,10 @@ import com.codedifferently.lesson26.library.MediaItem;
 import com.codedifferently.lesson26.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +30,20 @@ public class MediaItemsController {
   @GetMapping("/items")
   public ResponseEntity<GetMediaItemsResponse> getItems() {
     Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    if (items.isEmpty()) {
+      ResponseEntity.noContent();
+    }
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return ResponseEntity.ok(response);
   }
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<MediaItemResponse> getItemById(@PathVariable("id") UUID id) {
+
+    System.out.println(id.toString());
+
+    Set<MediaItem> items = library.search(SearchCriteria.builder().id(id.toString()).build());
+    Optional<MediaItem> matchedItem =
+        items.stream().filter(item -> item.getId().equals(id)).findFirst();
+    System.out.println("items");
 }
