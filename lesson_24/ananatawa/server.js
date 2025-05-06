9ddea7b2cb24728a740370210fcc311b89a38f37
Anthony D. Mays <anthony@morganlatimer.com>
2025-05-06 03:07:08 +0000

    feat: serves static files properly
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_24/ananatawa/server.js
+++ lesson_24/ananatawa/server.js
@@ -3,23 +3,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Home page
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to My Site</h1><p><a href="/contact">Contact Us</a></p>');
 });
-
-// Contact form
-app.get('/contact', (req, res) => {
-  res.send(`
-    <h2>Contact Us</h2>
-    <form action="/contact" method="POST">
-      <label>Name: <input type="text" name="name" /></label><br/>
-      <label>Message: <textarea name="message"></textarea></label><br/>
-      <button type="submit">Send</button>
-    </form>
-  `);
-}); 
 
 // Handle form submission
 app.post('/contact', (req, res) => {
