b88a74ac73ee8ded939fecd06779238de89cfb6e
Anthony D. Mays <anthony@morganlatimer.com>
2025-05-06 02:42:13 +0000

    chore: fix file paths
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_24/RMillerCDclone/index.js
+++ lesson_24/RMillerCDclone/index.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(cors());  // Enable CORS globally
 
 //Serve static files from the 'RMillerCDclone' directory
-app.use(express.static(path.join(__dirname, 'RMillerCDclone')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the contact form at the root
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'RMillerCDclone'));  
+    res.sendFile(path.join(__dirname, 'public'));  
 });
 
 // Handle form submission
