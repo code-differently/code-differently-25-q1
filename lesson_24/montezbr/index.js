a40dcb3189ad898ece4787af115ee9520664292e
Anthony D. Mays <anthony@morganlatimer.com>
2025-05-06 03:18:32 +0000

    fix: gets form submit working
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_24/montezbr/index.js
+++ lesson_24/montezbr/index.js
@@ -3,10 +3,18 @@ const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     console.log('Here')
     res.render('index')
 })
+
+app.post('/contact-form', (req, res) => {
+    const { firstname, email, message } = req.body;
+    res.send(`<h1>Thank you ${firstname} for your message: ${message}. We will contact you at ${email}</h1>`);
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
