51f662407add91e3e67e4c26407a9980bc7b2314
Meiko-S22 <mdstephens251@gmail,.com>
2025-05-08 20:11:54 +0000

    feat:  modifies programlist and adss new program page

---
+++ lesson_27/meikostephens/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.scss';
+import { Outlet } from 'react-router-dom';
+import { Home } from './pages/Home';
+import {Footer} from './components/footer';
+import { Header } from './components/header';
+
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="main">
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </>
+    
+  );
+}
+
+export default App;
