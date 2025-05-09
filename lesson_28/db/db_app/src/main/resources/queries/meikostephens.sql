6e3960a6df5b2af00db3329b8e3e36909a1dc3d5
Meiko-S22 <mdstephens251@gmail,.com>
2025-05-09 20:55:29 +0000

    feat:adds meikostephens.sql script

---
+++ lesson_28/db/db_app/src/main/resources/queries/meikostephens.sql
@@ -0,0 +1,25 @@
+
+SELECT 
+COUNT(*) As count
+FROM media_items
+GROUP BY type;
+
+SELECT 
+SUM(pages) as total_pages_checked_out
+FROM checked_out_items c 
+JOIN media_items m
+ON c.item_id = m.id;
+
+SELECT 
+g.email,
+g.name, 
+GROUP_CONCAT(c.item_id) AS checked_out_items
+FROM
+guests g 
+LEFT JOIN
+checked_out_items c ON g.email = c.email
+GROUP BY
+g.email, g.name, g.type;
+
+
+
