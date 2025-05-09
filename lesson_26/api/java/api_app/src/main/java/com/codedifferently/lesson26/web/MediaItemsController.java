cf938822592a6920534cbae50d80049447ccd050
Evan Philakhong <evanthegamer2004@gmail.com>
2025-05-09 17:15:59 +0000

    fix: changed RequestBody import from swagger to spring

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -1,25 +1,22 @@
 package com.codedifferently.lesson26.web;
 
+import com.codedifferently.lesson26.library.Librarian;
+import com.codedifferently.lesson26.library.Library;
+import com.codedifferently.lesson26.library.MediaItem;
+import com.codedifferently.lesson26.library.exceptions.MediaItemCheckedOutException;
+import com.codedifferently.lesson26.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.codedifferently.lesson26.library.Librarian;
-import com.codedifferently.lesson26.library.Library;
-import com.codedifferently.lesson26.library.MediaItem;
-import com.codedifferently.lesson26.library.exceptions.MediaItemCheckedOutException;
-import com.codedifferently.lesson26.library.search.SearchCriteria;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @CrossOrigin
@@ -54,14 +51,15 @@ public class MediaItemsController {
   }
 
   @PostMapping("/items")
-  public ResponseEntity<CreateMediaItemResponse> postItem(@RequestBody CreateMediaItemRequest request) {
+  public ResponseEntity<CreateMediaItemResponse> postItem(
+      @RequestBody CreateMediaItemRequest request) {
     MediaItem newItem = MediaItemRequest.asMediaItem(request.getItem());
 
     library.addMediaItem(newItem, librarian);
 
     MediaItemResponse itemResponse = MediaItemResponse.from(newItem);
     CreateMediaItemResponse response = CreateMediaItemResponse.builder().item(itemResponse).build();
-    
+
     return ResponseEntity.ok(response);
   }
 
@@ -72,7 +70,7 @@ public class MediaItemsController {
     if (items.isEmpty()) {
       return ResponseEntity.notFound().build();
     }
-  
+
     MediaItem itemToDelete = items.iterator().next();
 
     try {
