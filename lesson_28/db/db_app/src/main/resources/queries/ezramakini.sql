aef2efa94a5251a65e6fc03f1d53917bf92e3a3b
enyabuti <ezramakini123@gmail.com>
2025-05-09 21:08:00 +0000

    feat: adds SQL queries for media items and guest checkouts

---
+++ lesson_28/db/db_app/src/main/resources/queries/ezramakini.sql
@@ -0,0 +1,22 @@
+SELECT COUNT(*) AS total_media
+FROM media_items
+GROUP BY type
+ORDER BY type;
+
+
+SELECT SUM(pages) AS total_pages
+FROM checked_out_items x
+JOIN media_items y ON y.id = x.item_id;
+
+
+SELECT 
+    
+    g.name AS guest_name,
+    g.email AS guest_email,
+    g.type AS guest_type,
+    coi.item_id AS check_id,
+    coi.email,
+    coi.due_date
+FROM guests g
+LEFT JOIN checked_out_items coi ON g.email = coi.email
+LIMIT 5;
