8a3c5461accb5819367f2884837f279c841f5002
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-05-14 14:32:03 -0400

    chore: update import

--- lesson_25/davidadenaike/src/main.tsx
+++ lesson_25/davidadenaike/src/main.tsx
@@ -4,8 +4,8 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import NewProgram from './components/newPrograms/NewProgram.tsx';
 
-import {NewProgram} from './components/newPrograms/NewProgram.tsx';
 import './index.scss';
 
 const queryClient = new QueryClient();
