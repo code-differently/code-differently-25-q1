545bae9c9426e45012631a52ccac872b8df0b019
VicenteVigueras <vicente.viguerassilva@gmail.com>
2025-05-31 21:05:43 +0000

    chore: remove lesson_17 files to keep solely lesson_27 content

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/exceptions/InvalidBusinessAccountException.java
+++
@@ -1,7 +0,0 @@
-package com.codedifferently.lesson17.bank.exceptions;
-
-public class InvalidBusinessAccountException extends RuntimeException {
-  public InvalidBusinessAccountException(String message) {
-    super(message);
-  }
-}
