6e8fddcd51dd39a7a63540c5a9adb3ad48cd4c73
VicenteVigueras <vicente.viguerassilva@gmail.com>
2025-05-31 18:09:51 +0000

    feat: insert values to library users table

--- lesson_28/db/db_app/src/main/resources/queries/vicente.sql
+++ lesson_28/db/db_app/src/main/resources/queries/vicente.sql
@@ -19,3 +19,11 @@ CREATE TABLE library_users (
     last_name VARCHAR(255),
     password VARCHAR(255)
 );
+
+INSERT INTO library_users (user_id, email, first_name, last_name, password) VALUES
+('550e8400-e29b-41d4-a716-446655440000', 'vicente@test.com', 'Vicente', 'Test', '$2a$10$7QdB9JXzXTZr1NmVvl3poOT8bq/tBzSvWj7cXzDYPnS6X7CayVuR6'),
+('6c9e34ff-57a1-4f3a-9f09-0a3e7461df7a', 'kobe@test.com', 'Kobe', 'Bryant', '$2a$10$zRUj9hULhZlUz3EK2ky9W.B7IYqJS/8RqDhL6FfRMLV7e9UK9uRzO'),
+('a7d1f0c5-7d42-4a89-bf18-962dd0c6a1cd', 'michael@example.com', 'Michael', 'Jordan', '$2a$10$JqwHo6iW4aB7B7A0sXPuBOu9k6fi9i5cz2HgL7tss62tWcyeKkpqG'),
+('2f1d38f4-1692-4d28-958e-8a71c792f239', 'lebron@example.com', 'Lebron', 'James', '$2a$10$U1FQtWZdyq6guoZq6k3L6u2nZ8kgWqW6UlIYQGeF3By6F5XLZbP5e'),
+('b3c47289-ff23-4ee1-b3a1-0c9e271ac9b4', 'stephen@example.com', 'Steph', 'Curry', '$2a$10$Q9rZw3J6RRIwJtzPf4X0fej4KY6iHLFej1ZJXJZ6nTrYp1MZNoeKa');
+
